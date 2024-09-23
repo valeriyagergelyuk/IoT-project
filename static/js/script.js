e8b19f11c8909a325a536e3ed61af4bea7b4cfbc
valeriyagergelyuk <horse.shoe@outlook.com>
2024-09-23 10:42:03 -0400

    click on image to change the light

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,17 @@
 lightSwitch = "off";
 
 function toggleLED() {
-    let ledState = document.getElementById('switch').checked ? 'ON' : 'OFF';
+    //let ledState = document.getElementById('switch').checked ? 'ON' : 'OFF';
+
+    let ledState;
+    if(document.getElementById('lightSwitch').getAttribute('src')=='../static/images/offSwitch.jpg'){
+        ledState = 'ON';
+        //changeImages(ledState);
+    }if(document.getElementById('lightSwitch').getAttribute('src')=='../static/images/onSwitch.jpg'){
+        ledState = 'OFF';
+        //changeImages(ledState);
+    }
+    changeImages(ledState);
     fetch('/toggle_led', {
         method: 'POST',
         headers: {
@@ -10,11 +20,10 @@ function toggleLED() {
         body: JSON.stringify({ state: ledState }),
     });
     document.getElementById('ledStatus').innerHTML = ledState === 'ON' ? 'LED is ON' : 'LED is OFF';
-    changeImages();
+    
 }
 
-function changeImages(){
-    let ledState = document.getElementById('switch').checked ? 'ON' : 'OFF';
+function changeImages(ledState){
      if(ledState === "OFF"){
          document.getElementById('lightSwitch').src='../static/images/offSwitch.jpg';
          document.getElementById('lightbulb').src='../static/images/lightOff.png';
