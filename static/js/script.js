4dfb052cb3e9056e8fdef2d2cb457785a8a067f8
Petros-Chris <you@workpls.com>
2024-09-23 10:47:43 -0400

    cleaned code

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,13 @@
-lightSwitch = "off";
-
 function toggleLED() {
-    //let ledState = document.getElementById('switch').checked ? 'ON' : 'OFF';
-
     let ledState;
     if(document.getElementById('lightSwitch').getAttribute('src')=='../static/images/offSwitch.jpg'){
         ledState = 'ON';
-        //changeImages(ledState);
-    }if(document.getElementById('lightSwitch').getAttribute('src')=='../static/images/onSwitch.jpg'){
+    }
+    if(document.getElementById('lightSwitch').getAttribute('src')=='../static/images/onSwitch.jpg'){
         ledState = 'OFF';
-        //changeImages(ledState);
     }
     changeImages(ledState);
+    
     fetch('/toggle_led', {
         method: 'POST',
         headers: {
@@ -19,8 +15,6 @@ function toggleLED() {
         },
         body: JSON.stringify({ state: ledState }),
     });
-    document.getElementById('ledStatus').innerHTML = ledState === 'ON' ? 'LED is ON' : 'LED is OFF';
-    
 }
 
 function changeImages(ledState){
